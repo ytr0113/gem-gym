5dfd37dbf9745fc44f6cee2f5a057067f2385358
ytr0113 <kagekaze.hms@gmail.com>
2026-01-12 01:42:23 +0900

    add deploy.yml

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: false,
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   modules: ['@nuxtjs/tailwindcss', '@nuxtjs/supabase'],
